b520cd1317439b38994f5d7b32d3afd20a6103f8
Khachatur <khachkara@gmail.com>
2023-04-11 09:57:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,17 @@
-# https://github.com/Anastasiamay
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_1samp
+
+from scipy.stats import permutation_test
 
 chat_id = 42791670 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> bool:
-    alpha = 0.01
-    t_stat, p_value = ttest_1samp(x, 300)
-    return p_value/2 < alpha and t_stat < 0
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    
+    effect = permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
+                 vectorized=True, 
+                 n_resamples=1000,
+                 alternative='greater').pvalue < 0.01
+    return effect # Ваш ответ, True или False
